2d798a983df62a9f9bb7a7fac82f7f5888fb3e33
MCOdin5569 <mail@mcodin5569.de>
2025-07-02 14:54:45 +0200

    Updated Prefix

--- src/main/java/org/mcodin5569/nickName/Prefix.java
+++ src/main/java/org/mcodin5569/nickName/Prefix.java
@@ -4,6 +4,6 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 
 public class Prefix {
-    public static final String RAW = "<gray>» <#CC33FF>ᴀ<#A933FF>ɴ<#8633FF>ᴛ<#6333FF>ɪ<#6333FF>ʟ<#6333FF>ᴀ<#6333FF>ɢ <gray>| <white>";
+    public static final String RAW = "<gray>» <gradient:#CC33FF:#6333ff>ɴɪᴄᴋ</gradient> <gray>| <white>";
     public static final Component PREFIX = MiniMessage.miniMessage().deserialize(RAW);
 }
